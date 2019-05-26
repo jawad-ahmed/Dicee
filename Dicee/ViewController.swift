06f880950198003349f681bfc32ac2e9d3f5aad2
Jawad <8149510+jawad-ahmed@users.noreply.github.com>
2019-05-26 22:58:30 +0100

    Progammatically update dice display

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
 
@@ -26,6 +27,8 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
