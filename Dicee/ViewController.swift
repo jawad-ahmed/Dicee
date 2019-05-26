12ad814bf32ae4bdb7204147ff7a59ddf1e42c30
Jawad <8149510+jawad-ahmed@users.noreply.github.com>
2019-05-26 18:06:00 +0100

    Initial commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Jawad Ahmed on 26/05/2019.
+//  Copyright © 2019 Jawad Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    
+    }
+    
+}
+
