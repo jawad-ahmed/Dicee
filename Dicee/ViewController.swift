aa3dc10c849037a12c669aa4fee6f5772a28aacb
Jawad <8149510+jawad-ahmed@users.noreply.github.com>
2019-05-27 04:22:31 +0100

    DRY principle added
    Made the updating dice images into a function so it can be called during app load as well so the dices are random on application start

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -20,9 +20,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateDiceImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
