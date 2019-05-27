88c51ed5049b53532a2e33f14269477ae4b1c5f0
Jawad <8149510+jawad-ahmed@users.noreply.github.com>
2019-05-27 04:44:12 +0100

    Motion detection added
    On shake dice is rolled randomly

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -27,8 +27,11 @@ class ViewController: UIViewController {
         updateDiceImages()
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
     func updateDiceImages() {
-        
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
